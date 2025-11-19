511511e350fbfaa730f03571164ed9d2912db942
Johan Castaño <jach_1997@hotmail.com>
2025-11-19 09:59:46 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pragma Test Ionic',
+  webDir: 'www'
+};
+
+export default config;
